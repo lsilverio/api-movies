ca784bd274b6b2fd3900bd50485a48f161f56582
Lucas Silvério <sdp.silverio@gmail.com>
2023-09-16 17:03:09 -0300

    Modificação do teste

--- src/test/java/br/com/teste/backend/controller/MovieControllerTest.java
+++ src/test/java/br/com/teste/backend/controller/MovieControllerTest.java
@@ -1,23 +1,14 @@
 package br.com.teste.backend.controller;
 
-import br.com.teste.backend.dto.ResponseDto;
-import br.com.teste.backend.service.MovieService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -27,41 +18,14 @@ public class MovieControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @MockBean
-    private MovieService movieService;
-
-    @MockBean
-    private ResponseDto mockResponse;
-
-    @BeforeEach
-    public void setup() throws JsonProcessingException {
-        mockResponse = getResponseDto();
-    }
-
     @Test
     public void testCalculateIntervals() throws Exception {
-        when(movieService.calculateIntervals()).thenReturn(mockResponse);
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/movies/prizes")
+        mockMvc.perform(MockMvcRequestBuilders.get("/movies/prizes")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andReturn();
-
-        String contentAsString = result.getResponse().getContentAsString();
-        ResponseDto responseDto = objectMapper.readValue(contentAsString, ResponseDto.class);
-        assertNotNull(responseDto);
-        assertNotNull(responseDto.getMin());
-        assertNotNull(responseDto.getMax());
-        verify(movieService, times(1)).calculateIntervals();
-    }
-
-    private ResponseDto getResponseDto() throws JsonProcessingException {
-        String json = "{\"min\":[{\"producer\":\"Producer 1\",\"interval\":6,\"previousWin\":1995,\"followingWin\":2001}],\"max\":[{\"producer\":\"Producer 1\",\"interval\":14,\"previousWin\":2001,\"followingWin\":2015}]}";
-        return objectMapper.readValue(json, ResponseDto.class);
+                .andExpect(jsonPath("$.min[0].interval").value(6))
+                .andExpect(jsonPath("$.max[0].interval").value(6));
     }
 
 }
